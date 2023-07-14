b0fe4162a9b1b2ab702b20e90af0c206e0ea2a79
Nixhazel <neolokor1@gmail.com>
2023-07-15 00:41:48 +0100

    added status components

---
+++ src/components/statusComponents/Notoperational.tsx
@@ -0,0 +1,13 @@
+
+const Notoperational = () => {
+	return (
+		<div className='flex justify-center rounded-lg items-center gap-10 border-2 border-[#FF8195] h-[70px] w-full bg-[#FFF2F2] '>
+			<img src='sad-face.svg' alt='smile' className=' h-10' />
+			<p className='font-semibold text-lg p-0 m-0'>
+				Some systems are experiencing Issues
+			</p>
+		</div>
+	);
+};
+
+export default Notoperational;
---
+++ src/components/statusComponents/Operational.tsx
@@ -0,0 +1,13 @@
+
+const Operational = () => {
+	return (
+		<div className='flex justify-center rounded-lg items-center gap-10 border-2 border-[#12CD9C] h-[70px] w-full bg-[#E7FAF5] '>
+			<img src='in-smile-face-green.svg' alt='smile' className='' />
+			<p className='font-semibold text-lg p-0 m-0'>
+				All systems are Operational
+			</p>
+		</div>
+	);
+};
+
+export default Operational;
